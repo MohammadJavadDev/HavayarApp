0f56bf7df4b6440e195218677a7e688bdc74c106
MohammadJavad <52397743+MohammadJavadDev@users.noreply.github.com>
2025-12-22 11:03:57 +0330

    Create EntityActionDelegate.cs with delegate definition

---
+++ Data/Actions/EntityActionDelegate.cs
@@ -0,0 +1,10 @@
+// Generated on 2025-12-22 07:31:09 UTC
+
+namespace HavayarApp.Data.Actions
+{
+    /// <summary>
+    /// Delegate for entity action handling
+    /// </summary>
+    /// <param name="entity">The entity being acted upon</param>
+    public delegate void EntityActionDelegate(object entity);
+}
