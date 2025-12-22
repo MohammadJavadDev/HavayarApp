e2c4b99ea26bac1f75c4dc89017f90ea95771d8d
MohammadJavad <52397743+MohammadJavadDev@users.noreply.github.com>
2025-12-22 11:09:40 +0330

    Create ActionRegistry static class in Data/Actions directory

---
+++ Data/Actions/ActionRegistry.cs
@@ -0,0 +1,10 @@
+namespace HavayarApp.Data.Actions
+{
+    /// <summary>
+    /// ActionRegistry is a static class responsible for managing and registering application actions.
+    /// </summary>
+    public static class ActionRegistry
+    {
+        // Action registration logic will go here
+    }
+}
